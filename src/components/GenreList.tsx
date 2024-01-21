99325bf98d0a561e202c043ca70c4856a5655220
dexgapuz <dxgapuz@gmail.com>
2024-01-21 23:02:26 +0800

    added loader for genrelist

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,25 +1,31 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import { useGenres } from '../hooks/UseGenres';
 import { getCropedImageUrl } from '../services/ImageUrl';
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id}>
-          <HStack>
-            <Image
-              src={getCropedImageUrl(genre.image_background)}
-              boxSize="32px"
-              borderRadius={8}
-              paddingY="5px"
-            ></Image>
-            <Text fontSize="large">{genre.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && <Spinner />}
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id}>
+            <HStack>
+              <Image
+                src={getCropedImageUrl(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+                paddingY="5px"
+              ></Image>
+              <Text fontSize="large">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
